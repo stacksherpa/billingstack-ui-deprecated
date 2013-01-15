80e56ac7d17d1ff34a7c46836e0bc0ff8df870ca
woorea <luis@woorea.es>
2013-01-15 01:43:20 +0100

    display errors in ui

--- grails-app/controllers/com/billingstack/ApplicationController.groovy
+++ grails-app/controllers/com/billingstack/ApplicationController.groovy
@@ -3,48 +3,59 @@ package com.billingstack
 import com.ning.http.client.*
 
 class ApplicationController {
-	
-		def grailsApplication
+  
+    def grailsApplication
 
-		def http = new AsyncHttpClient()
+    def http = new AsyncHttpClient()
 
     def signUp() {
       if(request.post) {
-				def builder = new groovy.json.JsonBuilder()
-				def slurper = new groovy.json.JsonSlurper()
-				builder.call([
-					name : params.name,
-					language : params.language,
-					currency : params.currency,
-					user :[
-						username : params.user.username,
-						password : params.user.password
-					]
-				])
-				def response = http.preparePost(grailsApplication.config.billingstack.endpoint+'/merchants')
-					.setBody(builder.toString())
-					.execute()
-					.get()
-				slurper.parseText(response.responseBody)
-				redirect(action : "signIn")
+        try {
+          def builder = new groovy.json.JsonBuilder()
+          def slurper = new groovy.json.JsonSlurper()
+          builder.call([
+            name : params.name,
+            language : params.language,
+            currency : params.currency,
+            user :[
+              username : params.user.username,
+              password : params.user.password
+            ]
+          ])
+          def response = http.preparePost(grailsApplication.config.billingstack.endpoint+'/merchants')
+            .setBody(builder.toString())
+            .execute()
+            .get()
+          slurper.parseText(response.responseBody)
+          redirect(action : "signIn")
+        } catch(e) {
+          flash.error = e.message
+        }
       }
     }
     
     def signIn() {
       if(request.post) {
-				def builder = new groovy.json.JsonBuilder()
-				def slurper = new groovy.json.JsonSlurper()
-        builder.call([
-						username : params.username,
-						password : params.password
-				])
-				def response = http.preparePost(grailsApplication.config.billingstack.endpoint+'/authenticate')
-					.setBody(builder.toString())
-					.execute()
-					.get()
-				session.access = slurper.parseText(response.responseBody)
-				println session.access
-        redirect(controller : "merchant")
+        try {
+          def builder = new groovy.json.JsonBuilder()
+          def slurper = new groovy.json.JsonSlurper()
+          builder.call([
+              username : params.username,
+              password : params.password
+          ])
+          def response = http.preparePost(grailsApplication.config.billingstack.endpoint+'/authenticate')
+            .setBody(builder.toString())
+            .execute()
+            .get()
+					if(response.statusCode == 200) {
+						session.access = slurper.parseText(response.responseBody)
+	          redirect(controller : "merchant")
+					} else {
+						throw new RuntimeException(slurper.parseText(response.responseBody).error)
+					}
+        } catch(e) {
+          flash.error = e.message
+        }
       }
     }
     
